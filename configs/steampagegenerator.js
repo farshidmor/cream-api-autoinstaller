e4084302061fcdac0acd1e93f16f82e02522afee
Robert Pendell <shinji257@users.noreply.github.com>
2019-04-23 12:23:16 -0400

    Update to current cream api configs
    Added a bunch of new options.  Many are tagged inactive.  Removed a depreciated one.  Added steam_misc section to config that is generated.  Setup for Cream API 3.4.1.0

--- configs/steampagegenerator.js
+++ configs/steampagegenerator.js
@@ -50,7 +50,7 @@ function generateFileText(options) {
 	var data = {
 		steam: {
 			appid: {
-				value:"0",
+				value: "0",
 				description: "App id of game\n(http://store.steampowered.com/app/%appid%/)",
 				active: true
 			},
@@ -60,32 +60,27 @@ function generateFileText(options) {
 				active: false
 			},
 			unlockall: {
-				value:"false",
+				value: "false",
 				description: "Enable/disable automatic DLC unlock. Default option is set to \"false\".\nKeep in mind that this option is highly experimental and won't work if the game wants to call each DLC by index.",
 				active: true
 			},
 			orgapi: {
-				value:"steam_api_o.dll",
+				value: "steam_api_o.dll",
 				description: "Original Valve's steam_api.dll. Default is \"steam_api64_o.dll\".",
 				active: true
 			},
 			orgapi64: {
-				value:"steam_api64_o.dll",
+				value: "steam_api64_o.dll",
 				description: "Original Valve's steam_api64.dll. Default is \"steam_api64_o.dll\".",
 				active: true
 			},
 			extraprotection: {
-				value:"false",
+				value: "false",
 				description: "Enable/disable extra protection bypasser. Default is \"false\".",
 				active: true
 			},
-			forceuserdatafolder: {
-				value:"true",
-				description: "This option will force the usage of the default Steam user data folder. Default is \"true\".",
-				active: false
-			},
 			forceoffline: {
-				value:"false",
+				value: "false",
 				description: "This option will force the usage of the default Steam user data folder. Default is \"true\".",
 				active: true
 			},
@@ -94,47 +89,109 @@ function generateFileText(options) {
 				description: "Some games are checking for the low violence presence. Default is \"false\".",
 				active: false
 			},
+			installdir: {
+				value: "..\\",
+				description: "Installation path for the game. Maximum number of parent directories: 5 (..\..\..\..\..\). Default is the path to current working directory.",
+				active: false
+			},
+			dlcasinstalldir: {
+				value: "false",
+				description: "Use DLC id as the appended installation directory. e.g. <install_directory>\480 Default is \"true\".",
+				active: false
+			},
+			purchasetimestamp: {
+				value: "0",
+				description: "Purchase timestamp for the DLC (http://www.onlineconversion.com/unix_time.htm). Default is \"0\" (1970/01/01).",
+				active: false
+			},
+			wrappermode: {
+				value: "false",
+				description: "Turn on the wrapper mode. Default is \"false\".",
+				active: true
+			}
+		},
+		steam_misc: {
 			disableuserinterface: {
 				value:"false",
 				description: "Disables the internal SteamUser interface handler. Default is \"false\".\nDoes have an effect on the games that are using the license check for the DLC/application.",
 				active: true
 			},
 			disableutilsinterface: {
-				value:"false",
+				value: "false",
 				description: "Disables the internal SteamUtils interface handler. Default is \"false\".\nDoes have an effect on the games that are checking for the actual AppId (only matters when \"wrappermode\" is set to \"true\").",
 				active: true
 			},
-			wrappermode: {
-				value:"false",
-				description: "Turn on the wrapper mode. Default is \"false\".",
+			disableregisterinterfacefuncs: {
+				value: "false",
+				description: "Disable the internal reserve hook of the \"Steam_RegisterInterfaceFuncs\" function. Default is \"false\".",
 				active: true
+			},
+			unlockparentalrestrictions: {
+				value: "false",
+				description: "Unlock/Lock Steam parental restrictions. Default is \"true\".",
+				active: false
+			},
+			steamid: {
+				value: "0",
+				description: "SteamId64 to override. Note that this action could be risky ! This option can only work if \"disableuserinterface = false\".",
+				active: false
+			},
+			signaturebypass: {
+				value: "true",
+				description: "Bypass VAC signature check. Note that this action could be risky ! Default is \"false\".",
+				active: false
+			},
+			printbacktrace: {
+				value: "true",
+				description: "Print the backtrace for the vital API functions. Default is \"false\".",
+				active: false
 			}
 		},
 		steam_wrapper: {
 			newappid: {
-				value:"0",
+				value: "0",
 				description: "Application ID to override (used when the wrapper mode is on)",
 				active: true
 			},
 			wrapperremotestorage: {
-				value:"false",
+				value: "false",
 				description: "Use the internal storage system. Default is \"false\".",
 				active: true
 			},
 			wrapperuserstats: {
-				value:"false",
+				value: "false",
 				description: "Use the internal stats/achievements system. Default is \"false\".",
 				active: true
 			},
+			wrapperugc: {
+				value: "false",
+				description: "Use the internal workshop (UGC) system. Default is \"false\".",
+				active: true
+			},
 			saveindirectory: {
-				value:"false",
+				value: "false",
 				description: "Store the data in the current directory (incl. stats). By default the data will is stored at: %appdata%/CreamAPI/%appid%/\nDefault is \"false\".",
 				active: true
 			},
-			storestatscallback: {
-				value:"true",
-				description: "Disable/Enable a StoreStats callback. Takes effect only if \"wrapperuserstats\" is set to \"true\". Default is \"true\"",
+			forcefullsavepath: {
+				value: "false",
+				description: "Force the usage of a full save path instead of the relative one. Default is \"false\".",
+				active: true
+			},
+			disablecallbacks: {
+				value: "true",
+				description: "Disable/Enable a StoreStats callback. Takes effect only if \"wrapperuserstats\" is set to \"true\". Default is \"true\".",
 				active: false
+			},
+			storestatscallback: {
+				value: "true",
+				description: "Disable/Enable a StoreStats callback. Takes effect only if \"wrapperuserstats\" is set to \"true\". Default is \"true\".",
+				active: false
+			},
+			achievementscount: {
+				value: "0",
+				description: "Fixed achievements count. Some games can only work if this option is configured properly (e.g. Wolfenstein II). Default is \"0\".",
+				active: true
 			}
 		},
 		dlc: {
@@ -143,7 +200,7 @@ function generateFileText(options) {
 	var appdata = collectAppData();
 	data.steam.appid.value = appdata.appid;
 	data.dlc = collectDlc();
-	var serializeorder = ["steam","steam_wrapper","dlc"];
+	var serializeorder = ["steam","steam_misc","steam_wrapper","dlc"];
 	var includedescriptions = false;
 	if (options.descriptions == true) {
 		includedescriptions = true;
